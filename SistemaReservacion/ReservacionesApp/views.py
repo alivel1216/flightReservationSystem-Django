264fce486e5584a6d7f931d6df73b74dac0e6427
alivel1216 <70455689+alivel1216@users.noreply.github.com>
2020-09-06 16:53:18 -0500

    herencias
    Herencia del resto de plantillas

--- SistemaReservacion/ReservacionesApp/views.py
+++ SistemaReservacion/ReservacionesApp/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-#def home(request):
-    #return render(request, 'ReservacionesApp/home.html')
+def home(request):
+    return render(request, 'ReservacionesApp/index.html')
 
 def reservaciones(request):
     return render(request, 'ReservacionesApp/reservas.html')
 
 def pagos(request):
-    return HttpResponse("Pagos")
+    return render(request, 'ReservacionesApp/pagos.html')
 
 def consultas(request):
     return HttpResponse("Consultas")
