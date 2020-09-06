f092e9afd5268a755873fa3b8cf231de0953454f
alivel1216 <70455689+alivel1216@users.noreply.github.com>
2020-09-06 17:16:06 -0500

    links
    links funcionan correctamente

--- SistemaReservacion/ReservacionesApp/urls.py
+++ SistemaReservacion/ReservacionesApp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from ReservacionesApp import views
 urlpatterns = [
-    path('',views.reservaciones, name="Reservaciones"),
+    path('',views.home, name="Home"),
+    path('reservaciones',views.reservaciones, name="Reservaciones"),
     path('pagos',views.pagos, name="Pagos"),    
     path('consultas',views.consultas, name="Consultas"),
     path('vuelos',views.consultas, name="Vuelos"),
