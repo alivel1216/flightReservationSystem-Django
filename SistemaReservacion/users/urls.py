9f4bc2ff747eceaa2d35d069becbae861576408c
alivel1216 <70455689+alivel1216@users.noreply.github.com>
2020-09-10 17:35:38 -0500

    user-2
    arreglo de usuarios parte 2

--- SistemaReservacion/users/urls.py
+++ SistemaReservacion/users/urls.py
@@ -3,7 +3,8 @@ from users import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
-    path('registro/',views.registro, name="Registro"),
+    path('',views.registro, name="Registro"),
     path('login/',views.login, name="Login"),
 ]
--- SistemaReservacion/users/views.py
+++ SistemaReservacion/users/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from users.forms import createdUserForm
 # Create your views here.
 
 def registro(request):
-    context = {}
+    form =  UserCreationForm()
+    if request.method =='POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+    context = {'form':form}
     return render(request, 'users/registro.html', context)
 
 def login(request):
