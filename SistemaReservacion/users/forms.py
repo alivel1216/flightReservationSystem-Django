2a4a39a9baa2238147541d8212a2d9194040b8fc
alivel1216 <70455689+alivel1216@users.noreply.github.com>
2020-09-11 01:12:02 -0500

    login P1

--- SistemaReservacion/users/forms.py
+++ SistemaReservacion/users/forms.py
@@ -7,19 +7,6 @@ from django.contrib.auth.models import User
 class createdUserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = [
-            'username',
-            'first_name',
-            'last_name',
-            'email',
-            'password1',
-            'password2'
-        ]
-        widgets = {
-            'username': forms.TextInput(attrs={ 'class':'form-group  ','type':'text', 'placeholder':'Nombre de usuario'}),
-            'first_name': forms.TextInput(attrs={'class':'form-group  ', 'type':'text'}),
-            'last_name': forms.TextInput(attrs={'class':'form-group  ', 'type':'text'}),
-            'email': forms.TextInput(attrs={'class':'form-group  ','type':'email'}),
-            'password1': forms.TextInput(attrs={'class':'form-group  ','type':'password'}),
-            'password2': forms.TextInput(attrs={'class':'form-group  ','type':'password'})
-        }
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+
+ 
--- SistemaReservacion/users/views.py
+++ SistemaReservacion/users/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib import messages
 from users.forms import createdUserForm
 # Create your views here.
 
@@ -11,10 +15,23 @@ def registro(request):
         form = createdUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Se creo una cuenta para ' + user)
+            return redirect('Login')
             
     context = {'form':form}
     return render(request, 'users/registro.html', context)
 
-def login(request):
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(user)
+            return redirect('')
+
     context = {}
     return render(request, 'users/login.html', context)
